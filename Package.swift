c1aedcd67ab24e4085c586a6a0ba13b867cc1518
Andy French <andyf@canva.com>
2024-01-16 16:48:34 +1100

    Clean up

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,14 +8,11 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "thorvg-swift",
             targets: ["thorvg-swift"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "thorvg-swift",
             dependencies: ["thorvg"],
@@ -27,16 +23,15 @@ let package = Package(
             name: "thorvg",
             path: "src",
             exclude: [
-                "bindings/capi/tvgCapi.cpp", // TODO: Source of issue regarding duplicate symbols. Is this ok to remove?
                 "examples",
                 "bindings/wasm",
                 "loaders/external_jpg",
                 "loaders/external_png",
                 "loaders/external_webp",
-                "renderer/gl_engine", // TODO: Do we want a OpenGL engine?
-                "renderer/wg_engine", // TODO: Do we want a WebGPU engine?
+                "renderer/gl_engine",
+                "renderer/wg_engine",
                 "tools",
-            ], // TODO: Anything else we want to exclude?
+            ],
             publicHeadersPath: "bindings/capi",
             cxxSettings: [
                 .headerSearchPath("inc"),
@@ -54,7 +49,7 @@ let package = Package(
                 .headerSearchPath("renderer/sw_engine"),
                 .headerSearchPath("renderer/gl_engine"),
                 .headerSearchPath("savers/gif"),
-            ] // TODO: Also need to check other bits.
+            ]
         ),
         .testTarget(
             name: "thorvg-swift-tests",
