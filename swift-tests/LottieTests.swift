377c28fcd3f7114a28ffaa9d38dd32107589b222
Andy French <andyf@canva.com>
2024-01-24 13:09:25 +1100

    Moved size and crop into render function

--- swift-tests/LottieTests.swift
+++ swift-tests/LottieTests.swift
@@ -11,35 +11,20 @@ final class LottieTests: XCTestCase {
     // MARK: Initialiser tests
 
     func testInit_WithValidPath_ReturnsCorrectNumberOfFrames() throws {
-        let lottie = try Lottie(path: testLottieUrl.path, size: testSize)
+        let lottie = try Lottie(path: testLottieUrl.path)
 
         XCTAssertEqual(lottie.numberOfFrames, 180)
     }
 
     func testInit_WithValidPath_ReturnsCorrectDuration() throws {
-        let lottie = try Lottie(path: testLottieUrl.path, size: testSize)
+        let lottie = try Lottie(path: testLottieUrl.path)
 
         XCTAssertEqual(lottie.duration, CMTime(seconds: 3))
     }
 
-    func testInit_WithValidPath_ReturnsCorrectSize() throws {
-        let lottie = try Lottie(path: testLottieUrl.path, size: testSize)
-
-        XCTAssertEqual(lottie.size.height, 1024)
-        XCTAssertEqual(lottie.size.width, 1024)
-    }
-
-    func testInit_WithValidPath_AndCropped_ReturnsCorrectSize() throws {
-        let cropRect = CGRect(x: 0, y: 0, width: 1024, height: 1024)
-        let lottie = try Lottie(path: testLottieUrl.path, size: testSize, crop: cropRect)
-
-        XCTAssertEqual(lottie.size.height, 1024)
-        XCTAssertEqual(lottie.size.width, 1024)
-    }
-
     func testInit_WithInvalidPath_ThrowsError() {
         do {
-            _ = try Lottie(path: "", size: testSize)
+            _ = try Lottie(path: "")
 
             XCTFail("Expected failedToLoadLottieFromPath error to be thrown, but no error was thrown.")
         } catch {
@@ -49,14 +34,14 @@ final class LottieTests: XCTestCase {
 
     func testInit_WithValidString_Succeeds() throws {
         let animationJson = try NSMutableString(contentsOf: testLottieUrl, encoding: String.Encoding.utf8.rawValue) as String
-        let lottie = try Lottie(string: animationJson, size: testSize)
+        let lottie = try Lottie(string: animationJson)
 
         XCTAssertEqual(lottie.numberOfFrames, 180)
     }
 
     func testInit_WithInvalidString_ThrowsError() throws {
         do {
-            _ = try Lottie(string: "", size: testSize)
+            _ = try Lottie(string: "")
 
             XCTFail("Expected failedToLoadLottieFromString error to be thrown, but no error was thrown.")
         } catch {
@@ -67,19 +52,19 @@ final class LottieTests: XCTestCase {
     // MARK: Render tests
 
     func testRender_WithValidFrameIndex_BufferPopulatedWithContent() throws {
-        let lottie = try Lottie(path: testLottieUrl.path, size: testSize)
+        let lottie = try Lottie(path: testLottieUrl.path)
         var buffer = [UInt32](repeating: 0, count: Int(testSize.width * testSize.height))
 
-        try lottie.render(frameAt: 0, into: &buffer, stride: Int(testSize.width))
+        try lottie.render(frameAt: 0, into: &buffer, stride: Int(testSize.width), size: testSize)
 
         let bufferHasContent = buffer.contains { $0 != 0 }
         XCTAssertTrue(bufferHasContent, "Buffer should have non-zero values after rendering.")
     }
 
     func testRender_WithValidFrameIndex_ReturnedBufferPopulatedWithContent() throws {
-        let lottie = try Lottie(path: testLottieUrl.path, size: testSize)
+        let lottie = try Lottie(path: testLottieUrl.path)
 
-        let bufferPointer = try lottie.render(frameAt: 0, stride: Int(testSize.width))
+        let bufferPointer = try lottie.render(frameAt: 0, stride: Int(testSize.width), size: testSize)
 
         let buffer = UnsafeBufferPointer(start: bufferPointer, count: Int(testSize.width * testSize.height)).map { $0 }
 
@@ -88,11 +73,11 @@ final class LottieTests: XCTestCase {
     }
 
     func testRender_WithFrameIndexBelowBounds_ThrowsError() throws {
-        let lottie = try Lottie(path: testLottieUrl.path, size: testSize)
+        let lottie = try Lottie(path: testLottieUrl.path)
         var buffer = [UInt32](repeating: 0, count: Int(testSize.width * testSize.height))
 
         do {
-            try lottie.render(frameAt: -1, into: &buffer, stride: Int(testSize.width))
+            try lottie.render(frameAt: -1, into: &buffer, stride: Int(testSize.width), size: testSize)
 
             XCTFail("Expected frameIndexOutOfBounds error to be thrown, but no error was thrown.")
         } catch {
@@ -101,11 +86,11 @@ final class LottieTests: XCTestCase {
     }
 
     func testRender_WithFrameIndexAboveBounds_ThrowsError() throws {
-        let lottie = try Lottie(path: testLottieUrl.path, size: testSize)
+        let lottie = try Lottie(path: testLottieUrl.path)
         var buffer = [UInt32](repeating: 0, count: Int(testSize.width * testSize.height))
 
         do {
-            try lottie.render(frameAt: 180, into: &buffer, stride: Int(testSize.width))
+            try lottie.render(frameAt: 180, into: &buffer, stride: Int(testSize.width), size: testSize)
 
             XCTFail("Expected frameIndexOutOfBounds error to be thrown, but no error was thrown.")
         } catch {
--- swift/Lottie.swift
+++ swift/Lottie.swift
@@ -15,6 +15,12 @@ public enum LottieError: Error {
 /// Shorthand for the buffer type used to represent image pixel data.
 public typealias Buffer = UnsafeMutablePointer<UInt32>
 
+/// Shorthand for a pointer to an internal Lottie animation object
+private typealias Animation = OpaquePointer
+
+/// Shorthand for a pointer to an internal rendering canvas
+private typealias Canvas = OpaquePointer
+
 /// A class representing a Lottie animation, providing functionalities to load and render Lottie frames.
 public class Lottie {
     /// The number of frames in the Lottie animation.
@@ -23,22 +29,16 @@ public class Lottie {
     /// The duration of the Lottie animation.
     public let duration: CMTime
 
-    /// The size of the Lottie animation.
-    public let size: CGSize
+    /// The Lottie animation, used for manipulating and rendering frames.
+    private let animation: Animation?
 
-    /// An optional cropping rectangle to crop the animation.
-    /// If set, the animation will be cropped to the specified rectangle before rendering.
-    public let crop: CGRect?
-
-    private let animation: OpaquePointer?
-    private var canvas: OpaquePointer? = nil
+    /// The canvas used for rendering the animation frames, allowing reuse across renders.
+    private var canvas: Canvas? = nil
 
     /// Initializes a new Lottie instance from a file path.
     /// - Parameters:
     ///   - path: The file path of the Lottie animation.
-    ///   - size: The size to render the Lottie animation.
-    ///   - crop: An optional cropping rectangle to crop the animation before rendering.
-    public convenience init(path: String, size: CGSize, crop: CGRect? = nil) throws {
+    public convenience init(path: String) throws {
         let animation = tvg_animation_new()
         let picture = tvg_animation_get_picture(animation)
 
@@ -46,15 +46,13 @@ public class Lottie {
             throw LottieError.failedToLoadLottieFromPath
         }
 
-        try self.init(animation: animation, size: size, crop: crop)
+        try self.init(animation: animation)
     }
 
     /// Initializes a new Lottie instance from a string representing the animation.
     /// - Parameters:
     ///   - string: The string representing the Lottie animation.
-    ///   - size: The size to render the Lottie animation.
-    ///   - crop: An optional cropping rectangle to crop the animation before rendering.
-    public convenience init(string: String, size: CGSize, crop: CGRect? = nil) throws {
+    public convenience init(string: String) throws {
         let animation = tvg_animation_new()
         let picture = tvg_animation_get_picture(animation)
 
@@ -64,15 +62,73 @@ public class Lottie {
             throw LottieError.failedToLoadLottieFromString
         }
 
-        try self.init(animation: animation, size: size, crop: crop)
+        try self.init(animation: animation)
     }
 
-    private init(animation: OpaquePointer?, size: CGSize, crop: CGRect?) throws {
+    /// Initializes the Lottie instance with frame count and duration extracted from the animation pointer.
+    private init(animation: Animation?) throws {
         var numberOfFrames: Float = 0
         tvg_animation_get_total_frame(animation, &numberOfFrames)
 
         var duration: Float = 0
         tvg_animation_get_duration(animation, &duration)
+
+        self.animation = animation
+        self.numberOfFrames = Int(numberOfFrames)
+        self.duration = CMTime(seconds: Double(duration), preferredTimescale: 600)
+    }
+
+    /// Renders a Lottie frame into a new buffer.
+    /// - Parameters:
+    ///   - index: The index of the frame to render.
+    ///   - stride: The stride of the buffer.
+    /// - Returns: A buffer containing the rendered Lottie frame.
+    func render(frameAt index: Int, stride: Int, size: CGSize, crop: CGRect? = nil) throws -> Buffer {
+        let bufferSize = Int(size.width * size.height)
+        let buffer = Buffer.allocate(capacity: bufferSize)
+        buffer.initialize(repeating: 0, count: bufferSize)
+
+        try render(frameAt: index, into: buffer, stride: stride, size: size, crop: crop)
+        return buffer
+    }
+
+    /// Renders a Lottie frame into an existing buffer.
+    /// - Parameters:
+    ///   - index: The index of the frame to render.
+    ///   - buffer: The buffer to render the frame into.
+    ///   - stride: The stride of the buffer.
+    func render(frameAt index: Int, into buffer: Buffer, stride: Int, size: CGSize, crop: CGRect? = nil) throws {
+        guard index < numberOfFrames, index >= 0 else {
+            throw LottieError.frameIndexOutOfBounds
+        }
+
+        if canvas == nil {
+            canvas = try createCanvas(with: buffer, stride: stride, size: size)
+        }
+
+        prepareCanvasForRendering(frameIndex: index, size: size, crop: crop)
+        try renderCanvas()
+    }
+
+    /// Creates a new canvas for rendering, initializing the TVG engine and setting the target buffer.
+    private func createCanvas(with buffer: Buffer, stride: Int, size: CGSize) throws -> Canvas? {
+        guard tvg_engine_init(TVG_ENGINE_SW, 4) == TVG_RESULT_SUCCESS else {
+            throw LottieError.failedToInitialiseTvgEngine
+        }
+
+        let canvas = tvg_swcanvas_create()
+        tvg_swcanvas_set_target(canvas, buffer, UInt32(stride), UInt32(size.width), UInt32(size.height), TVG_COLORSPACE_ARGB8888)
+
+        let picture = tvg_animation_get_picture(animation)
+        tvg_canvas_push(canvas, picture)
+
+        return canvas
+    }
+
+    /// Prepares the canvas for rendering by setting the frame, adjusting the picture size, and optionally applying a crop.
+    private func prepareCanvasForRendering(frameIndex index: Int, size: CGSize, crop: CGRect?) {
+        tvg_canvas_clear(canvas, false, true)
+        tvg_animation_set_frame(animation, Float(index))
 
         let picture = tvg_animation_get_picture(animation)
         tvg_picture_set_size(picture, Float(size.width), Float(size.height))
@@ -89,66 +145,16 @@ public class Lottie {
             tvg_paint_set_composite_method(picture, cropShape, TVG_COMPOSITE_METHOD_CLIP_PATH)
         }
 
-        self.animation = animation
-        self.numberOfFrames = Int(numberOfFrames)
-        self.duration = CMTime(seconds: Double(duration), preferredTimescale: 600)
-        self.size = size
-        self.crop = crop
-    }
-
-    /// Renders a Lottie frame into a new buffer.
-    /// - Parameters:
-    ///   - index: The index of the frame to render.
-    ///   - stride: The stride of the buffer.
-    /// - Returns: A buffer containing the rendered Lottie frame.
-    func render(frameAt index: Int, stride: Int) throws -> Buffer {
-        let bufferSize = Int(size.width * size.height)
-        let buffer = Buffer.allocate(capacity: bufferSize)
-        buffer.initialize(repeating: 0, count: bufferSize)
-
-        try render(frameAt: index, into: buffer, stride: stride)
-        return buffer
-    }
-
-    /// Renders a Lottie frame into an existing buffer.
-    /// - Parameters:
-    ///   - index: The index of the frame to render.
-    ///   - buffer: The buffer to render the frame into.
-    ///   - stride: The stride of the buffer.
-    func render(frameAt index: Int, into buffer: Buffer, stride: Int) throws {
-        guard index < numberOfFrames, index >= 0 else {
-            throw LottieError.frameIndexOutOfBounds
-        }
-
-        if canvas == nil {
-            canvas = try createCanvas(with: buffer, stride: stride, size: size)
-        }
-
-        tvg_canvas_clear(canvas, false, true)
-        tvg_animation_set_frame(animation, Float(index))
-
-        let picture = tvg_animation_get_picture(animation)
         tvg_canvas_update_paint(canvas, picture)
+    }
 
+    /// Renders the prepared content of the canvas onto the actual canvas.
+    private func renderCanvas() throws {
         guard tvg_canvas_draw(canvas) == TVG_RESULT_SUCCESS else {
             throw LottieError.failedToDrawLottieFrameOntoCanvas
         }
 
         tvg_canvas_sync(canvas)
-    }
-
-    private func createCanvas(with buffer: Buffer, stride: Int, size: CGSize) throws -> OpaquePointer! {
-        guard tvg_engine_init(TVG_ENGINE_SW, 4) == TVG_RESULT_SUCCESS else {
-            throw LottieError.failedToInitialiseTvgEngine
-        }
-
-        let canvas = tvg_swcanvas_create()
-        tvg_swcanvas_set_target(canvas, buffer, UInt32(stride), UInt32(size.width), UInt32(size.height), TVG_COLORSPACE_ARGB8888)
-
-        let picture = tvg_animation_get_picture(animation)
-        tvg_canvas_push(canvas, picture)
-
-        return canvas
     }
 
     deinit {
