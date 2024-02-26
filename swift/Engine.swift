919c9dbf42dced50774c7b32cc718d89cdbdfee2
Andy French <andyf@canva.com>
2024-02-27 10:00:22 +1100

    Introduce internal Swift types

---
+++ swift/Engine.swift
@@ -0,0 +1,25 @@
+import CoreGraphics
+
+import thorvg
+
+/// A Swift wrapper for managing ThorVG's engine initialization and termination.
+public class Engine {
+    /// A default instance of `Engine`, running on the main thread.
+    public static let `default` = Engine()
+
+    /// Initializes the engine with a specified number of threads.
+    ///
+    /// Note: This defaults the number of threads to 0, which tells ThorVG to run on the main thread.
+    public init(numberOfThreads: Int = 0) {
+        tvg_engine_init(TVG_ENGINE_SW, UInt32(numberOfThreads))
+    }
+
+    /// Terminates the ThorVG engine, cleaning up resources.
+    public func terminate() {
+        tvg_engine_term(TVG_ENGINE_SW)
+    }
+
+    deinit {
+        terminate()
+    }
+}
